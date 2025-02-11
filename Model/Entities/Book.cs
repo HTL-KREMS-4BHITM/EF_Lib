d204ee972b0307bc3bf1b4b7fd9e60b3eb6f03ee
werwolfy <raphael.nigl@outlook.de>
2025-02-11 12:45:07 +0100

    [TASK] Renamed vallues from the entities

--- Model/Entities/Book.cs
+++ Model/Entities/Book.cs
@@ -3,44 +3,46 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Model.Entities;
 
-[Table(("Books"))]
-public class Book { 
+[Table("BOOKS")]
+public class Book
+{
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("ID")]
-    public int Id { get; set; } 
-    
+    public int Id { get; set; }
+
     [Required, StringLength(45)]
-    [Column("Title")]
-    public string Title { get; set; } 
-    
+    [Column("TITLE")]
+    public string Title { get; set; }
+
     [Required, StringLength(50)]
-    [Column("Authors")]
+    [Column("AUTHORS")]
     public string Author { get; set; }
-    
+
     [Required, Timestamp]
-    [Column("PublishedDate")]
+    [Column("PUBLISHEDDATE")]
     public DateTime PublishedDate { get; set; }
-    
+
     [Required, StringLength(30)]
     [Column("ISBN")]
-    public string ISBN { get; set; } 
-    
+    public string ISBN { get; set; }
+
     public BookDetails BookDetails { get; set; }
-    
+
     [Required]
-    [Column("BookDetailsID")]
+    [Column("BOOKDETAILSID")]
     public int BookDetailsId { get; set; }
-    
+
     public Author AuthorInit { get; set; }
-    
+
     [Required]
-    [Column("AuthorID")]
+    [Column("AUTHORID")]
     public int AuthorId { get; set; }
-} 
-public class NonFiction : Book { } 
-public class Novel : Book { } 
-public class Textbook : Book { } 
-public class Biography : Book { } 
-public class ScienceFiction : Book { } 
-public class Fantasy : Book { } 
-public class Mystery : Book { } 
+}
+
+public class NonFiction : Book { }
+public class Novel : Book { }
+public class Textbook : Book { }
+public class Biography : Book { }
+public class ScienceFiction : Book { }
+public class Fantasy : Book { }
+public class Mystery : Book { }
